b5c3b189feef01d782c4d6109d2269cd0b4b4248
devopslab2020 <devopslab2020@gmail.com>
2020-01-27 10:36:25 +0530

    SecondCommit

--- Add.java
+++ Add.java
@@ -1,9 +1,10 @@
-public class AddTwoIntegers {
+public class Add {
     public static void main(String[] args) {
         
         int first = 10;
         int second = 20;
-        int sum = first + second;
+        int third =30;  
+        int sum = first + second +third;
         System.out.println("The sum is: " + sum);
     }
 }
